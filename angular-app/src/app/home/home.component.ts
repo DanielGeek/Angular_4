781ba4e188889290874aa047718a2a4ef4032c49
DanielGeek <daniel30081990@gmail.com>
2018-01-01 20:41:01 -0400

    Agregado slide con ngFor

--- angular-app/src/app/home/home.component.ts
+++ angular-app/src/app/home/home.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  homeImageList = [
+    {image: "assets/images/nature/1.jpg", title: "Image 1", link: "videos/video-1"},
+    {image: "assets/images/nature/2.jpg", title: "Image 2", link: "videos/video-1"},
+    {image: "assets/images/nature/3.jpg", title: "Image 3", link: "videos/video-1"},
+    {image: "assets/images/nature/4.jpg", title: "Image 4", link: "videos/video-1"},
+    {image: "assets/images/nature/5.jpg", title: "Image 5", link: "videos/video-1"},
+    {image: "assets/images/nature/6.jpg", title: "Image 6", link: "videos/video-1"}
+  ]
   constructor() { }
 
   ngOnInit() {
+  }
+
+  preventNormal(event:MouseEvent){
+    console.log(event)
+    event.preventDefault()
+    alert("Working...")
   }
 
 }
